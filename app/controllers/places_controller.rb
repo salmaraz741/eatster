9f87e2da4b98f43f0a560e08418430bb834642be
salmaraz741 <salmaraz741@gmail.com>
2017-06-30 22:09:41 +0000

    Place detail page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,7 @@
 class PlacesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   def index
-    @places = Place.all
+    @places = Place.search(params[:term], params[:page])
   end
 
   def new
@@ -11,6 +11,10 @@ class PlacesController < ApplicationController
   def create
     current_user.places.create(place_params)
     redirect_to root_path
+  end
+
+  def show
+    @place = Place.find(params[:id])
   end
 
   private
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,11 @@
 class Place < ApplicationRecord
   belongs_to :user
+
+  def self.search(term, page)
+    if term
+      where('name LIKE ?', "%#{term}%").paginate(page: page, per_page: 2).order('id DESC')
+    else
+      paginate(page: page, per_page: 2).order('id DESC') 
+    end
+  end
 end
