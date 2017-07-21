91882250ba7ac4c2e57837901cb1b1847d07ffaa
salmaraz741 <salmaraz741@gmail.com>
2017-07-21 21:50:08 +0000

    latest changes

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -80,7 +80,7 @@ SimpleForm.setup do |config|
 
   # ID to add for error notification helper.
   # config.error_notification_id = nil
-
+  
   # Series of attempts to detect a default label method for collection.
   # config.collection_label_methods = [ :to_label, :name, :title, :to_s ]
 
