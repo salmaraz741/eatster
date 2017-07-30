8db2f18d1a8704fcfba57ca39ff5c2acb7d52f29
salmaraz741 <salmaraz741@gmail.com>
2017-07-30 06:32:06 +0000

    last commit

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -35,16 +35,16 @@ class PlacesController < ApplicationController
     @place = Place.find(params[:id])
 
     if @place.user != current_user
-      return render text: 'Not Allowed', status: :forbidden
+      return render :text => 'Not Allowed', :status => :forbidden
     end
 
     @place.update_attributes(place_params)
     if @place.valid?
       redirect_to root_path
     else
-      render :edit, status: :unprocessable_entity
+      render :edit, :status => :unprocessable_entity
     end
-  end 
+  end
 
   def destroy
     @place = Place.find(params[:id])
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,10 +1,8 @@
 class Place < ApplicationRecord
   belongs_to :user
-  has_many :comments
-  geocoded_by :address
-  after_validation :geocode
-  
-  validates :name, presence: true
+  validates :name, :length => { minimum:4 },:presence => true
+  validates :address, :length => { minimum:4 },:presence => true
+  validates :description, :presence => true
 end
 
   def self.search(term, page)
