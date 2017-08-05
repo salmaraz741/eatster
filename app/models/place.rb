cfbd1726cca7ce711ab535323df89f996290aff0
salmaraz741 <salmaraz741@gmail.com>
2017-08-05 06:54:13 +0000

    latest commit

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,7 @@
 class Place < ApplicationRecord
   belongs_to :user
+  geocoded_by :address
+  after_validation :geocode
   has_many :comments
   validates :name, :length => { minimum:4 },:presence => true
   validates :address, :length => { minimum:4 },:presence => true
