5d082c0969f6b126b749616dc47d93934a2bfa1b
salmaraz741 <salmaraz741@gmail.com>
2017-08-15 01:47:54 +0000

    Latest changes

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,7 +2,8 @@ class Place < ApplicationRecord
   belongs_to :user
   geocoded_by :address
   after_validation :geocode
-  has_many :comments, :memberships, :dependent => :delete_all
+  has_many :comments, :dependent => :delete_all
+  has_many :memberships, :dependent => :delete_all
   validates :name, :length => { minimum:4 },:presence => true
   validates :address, :length => { minimum:4 },:presence => true
   validates :description, :presence => true
